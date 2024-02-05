0dea7913a4b48f94bbc4d8c2d78fabe14b9d00cf
Mathijs Afman <afman.mathijs@gmail.com>
2024-02-06 00:44:41 +0100

    updated if else logic

--- main.py
+++ main.py
@@ -25,21 +25,23 @@ def main():
     # main loop
     try:
         old_uuid = ""
+        playing = False
         while True:
             # scan rfid tag
             print("Hold a tag near the reader")
             uuid = reader.read_id(timeout=5)
-            if uuid:
-                print("ID: ", uuid)
-            else: # stop playback
-                print("no tag found")
+            if playing and not uuid: # stop playback
+                print("no tag found, stopping playback")
                 if mpd_funcs.stop_playback(client):
+                    old_uuid = "" # reset old uuid so that album replays when it is placed again
                     print("stopped playback")
                 else:
                     print("something went wrong")
 
             # check if tag is new
-            if uuid != old_uuid and uuid is not None: # early exit
+            if uuid != old_uuid: # early exit
+                print("new tag detected, playing album")
+                playing = True
                 old_uuid = uuid
                 album_name = database[str(uuid)]
                 if mpd_funcs.play_album(album_name, client):
